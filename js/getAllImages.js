74865b08653ae287b08d2f80118ef5f704263759
srigiriharish <srigiriharish@gmail.com>
2018-04-17 01:43:08 +0530

    changes

--- js/getAllImages.js
+++ js/getAllImages.js
@@ -3,11 +3,12 @@ var folder = "images/content/gallery";
 $.ajax({
     url : folder,
     success: function (data) {
-        alert('Success');
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $(".gallery").append( "<img class='col-md-4 col-sm-6 col-xs-12 image-file' src='"+ folder + "/" + val +"'>" );
+                $(".gallery").append( "<img style='cursor: pointer;' class='col-md-4 col-sm-6 col-xs-12 image-file' data-original='"+ folder + "/" + val +"' src='"+ folder + "/" + val +"'>" );
             } 
         });
+        $('.gallery').viewer();
     }
 });
+
